341772758da2046e0a49cfdfce6f0086555f0e11
Eero Kauppi <eero.kauppi@stud.pointcollege.fi>
2016-04-20 10:24:55 +0300

    Perustettu Stadble projekti, lisätty tietokanta sekä julkaisu Azureen

---
+++ StaDBleWebApp/StaDBleWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StaDBleWebApp.Startup))]
+namespace StaDBleWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
